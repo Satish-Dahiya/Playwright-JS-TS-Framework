5e31411336c0b28319c3ab4af2733cd211f72e48
Satish-Dahiya <satishkd99automation@gmail.com>
2026-03-05 21:59:36 +0530

    Running framework in GIThub actions using playwright-run.yml file.

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -22,9 +22,9 @@ export default defineConfig({
       testEnvironment: 'PROD',
       embedAssets: true,
       embedAttachments: true,
-      outputFolder: 'playwright-html-Myreport',
+      outputFolder: 'playwright-html-report',
       minifyAssets: true,
-      startServer: true,
+      startServer: false,
     }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -33,7 +33,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     screenshot: 'on-first-failure',
     video: 'on',
     baseURL: 'https://naveenautomationlabs.com/opencart/index.php',
@@ -88,53 +88,53 @@ export default defineConfig({
       }
     },
 
-    {
-      name: 'Microsoft Edge',
-      use: {
-        channel: 'msedge',
-        viewport: null,
-        launchOptions: {
-          args: ['--start-maximized'],
-          ignoreDefaultArgs: ['--window-size=1280,720']
-        }
-      }
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: {
+    //     channel: 'msedge',
+    //     viewport: null,
+    //     launchOptions: {
+    //       args: ['--start-maximized'],
+    //       ignoreDefaultArgs: ['--window-size=1280,720']
+    //     }
+    //   }
+    // },
 
-    {
-      name: 'Chromium',
-      use: {
-        browserName: 'chromium',
-        viewport: { width: 1920, height: 1080 },
-        launchOptions: {
-          args: [],
-          ignoreDefaultArgs: ['--window-size=1280,720']
-        }
-      }
-    },
+    // {
+    //   name: 'Chromium',
+    //   use: {
+    //     browserName: 'chromium',
+    //     viewport: { width: 1920, height: 1080 },
+    //     launchOptions: {
+    //       args: [],
+    //       ignoreDefaultArgs: ['--window-size=1280,720']
+    //     }
+    //   }
+    // },
 
-    {
-      name: 'Firefox',
-      use: {
-        browserName: 'firefox',
-        viewport: { width: 1920, height: 1080 },
-        launchOptions: {
-          args: [],
-          ignoreDefaultArgs: ['--window-size=1280,720']
-        }
-      }
-    },
+    // {
+    //   name: 'Firefox',
+    //   use: {
+    //     browserName: 'firefox',
+    //     viewport: { width: 1920, height: 1080 },
+    //     launchOptions: {
+    //       args: [],
+    //       ignoreDefaultArgs: ['--window-size=1280,720']
+    //     }
+    //   }
+    // },
 
-    {
-      name: 'WebKit',
-      use: {
-        browserName: 'webkit',
-        viewport: { width: 1920, height: 1080 },
-        launchOptions: {
-          args: [],
-          ignoreDefaultArgs: ['--window-size=1280,720']
-        }
-      }
-    }
+    // {
+    //   name: 'WebKit',
+    //   use: {
+    //     browserName: 'webkit',
+    //     viewport: { width: 1920, height: 1080 },
+    //     launchOptions: {
+    //       args: [],
+    //       ignoreDefaultArgs: ['--window-size=1280,720']
+    //     }
+    //   }
+    // }
   ],
 
 
