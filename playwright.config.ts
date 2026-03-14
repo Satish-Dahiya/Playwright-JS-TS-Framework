85f460927e7c28bc8adf872580e4a6d349f5065e
Satish-Dahiya <satishkd99automation@gmail.com>
2026-03-14 15:40:59 +0530

    fixed html reporting in some config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
+    ['list'],
     ['allure-playwright'],
     ['playwright-html-reporter', { 
       testFolder: 'tests',
